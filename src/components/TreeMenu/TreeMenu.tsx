947897148953e3464c23010a1b9f9af655d5bfa3
Denis <takoiwebile@gmail.com>
2022-04-05 16:25:51 +0200

    fetch data

--- src/components/TreeMenu/TreeMenu.tsx
+++ src/components/TreeMenu/TreeMenu.tsx
@@ -1,5 +1,17 @@
+import { useEffect, useState } from 'react';
+
 import styles from './styles.module.css';
 
+type TreeNode = { title: string; nodes: TreeNode[] };
+
 export const TreeMenu = () => {
-  return <p className={styles.bold}>TreeMenu</p>;
+  const [data, setData] = useState<TreeNode[] | null>(null);
+
+  useEffect(() => {
+    fetch('./data.json')
+      .then(res => res.json())
+      .then(data => setData(data));
+  });
+
+  return data && <p className={styles.bold}>TreeMenu</p>;
 };
